b9f1036160c9ad8191ff33cf25653b745f1cccb3
Nick <nsagias@gmail.com>
2021-09-23 21:09:56 -0400

    add functionally to fetchCoordinates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,3 +10,16 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+
+fetchCoordsByIP('142.126.224.216',() => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  } 
+  console.log('It worked! Returned Coordinates:')
+});
+
+// Done: Require it in index.js
+// Done: For now, call the function and pass in our (IPv4) IP address string as the first argument to the function
+// For now, our callback can simply print out the values for error and data
--- iss.js
+++ iss.js
@@ -10,7 +10,7 @@
 
 const request = require('request');
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = (callback) => {
   // use request to fetch IP address from JSON API
   const req = 'https://api.ipify.org?format=json';
   request(req, (error, response, body) => {
@@ -35,6 +35,59 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Define the fetchCoordsByIP
+ * It should take in two arguments: ip (string) and callback
+ * Add the function to the object properties being exported from iss.js
+ * For now, it can have an empty body and do nothing
+ *
+ * "latitude":44,"longitude":-79,
+ */
+
+const fetchCoordsByIP = (ip, callback) => {
+  const req = 'https://freegeoip.app/json/';
+  // const req =  'https://freegeoip.app/json/invalideip'
+  request(req, (error, response, body) => {
+    if (error) {
+      // return console.log(error, null);
+      return callback(error, null);
+    }
+    
+
+    if (response.statusCode == 404) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+      // return console.log(Error(msg), null);
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+      // return console.log(Error(msg), null);
+    }
+    
+    let result = {};
+    const latitude =  JSON.parse(body).latitude;
+    const longitude = JSON.parse(body).longitude;
+    // check if value if response goes gthrough
+    // const longitude = undefined
+    if (!latitude || !longitude) {
+      // return console.log('either no latitute or longitude', null);
+      return callback('either no latitute or longitude', null);
+    }
+
+    result["latitude"] = latitude;
+    result["longitude"] = longitude;
+    // return console.log(null, result);
+    return callback(null, result);
+  });
+
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
 
 
